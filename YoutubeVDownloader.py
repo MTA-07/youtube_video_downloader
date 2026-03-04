b87f12de38fe07adca1079c119501b774d019b2c
Mehmet Turan Arslan <arslannmehmet392@gmail.com>
2026-03-04 22:17:02 +0300

    Kod guncellendi

--- YoutubeVDownloader.py
+++ YoutubeVDownloader.py
@@ -1,14 +1,16 @@
 import yt_dlp
+import os
+desktop = os.path.join(os.path.expanduser("~"), "Desktop")
 
 url = input("YouTube URL: ").strip()
 
 ydl_opts = {
-    "outtmpl": "%(title)s.%(ext)s",
+    "outtmpl": f"{desktop}/%(title)s.%(ext)s",
 }
 
 try:
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
-    print("Video indirildi!")
+    print(f"\nİşlem Tamam! Video Masaüstüne kaydedildi.")
 except Exception as e:
     print("Hata:", e)
